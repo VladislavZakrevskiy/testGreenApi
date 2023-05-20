410d403c88dd6a63a21cafc4571c8ff19c6393a2
vlad <zakrevskiyvlada@yandex.ru>
2023-05-20 18:07:54 +0400

    update store

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,8 +9,7 @@ const reducers = combineReducers({
 })
 
 export const store = configureStore({
-  reducer: reducers,
-  devTools: import.meta.env.NODE_ENV !== "production",
+  reducer: reducers, 
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({}).concat([chatApi.middleware]),
 });
 
